4655ab21590ba3df4f750566ceffe6b5169cd553
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-11-12 18:11:30 -0600

    initial setup

---
+++ testCLI.js
@@ -0,0 +1,12 @@
+import jwt, { jwtDecode } from "jwt-authn";
+
+const header = "";
+const payload = "";
+const signature = "";
+const jot = {};
+
+jot.header = header;
+jot.payload = payload;
+jot.signature = signature;
+
+jwtDecode();
